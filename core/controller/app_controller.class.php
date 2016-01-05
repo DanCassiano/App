7447864ed7eb51338d9365c74d7aad61f2c85649
Jordan <jordan@emporioht.com.br>
2016-01-05 00:42:53 -0200

    Adicao do namespace

---
+++ core/controller/app_controller.class.php
@@ -0,0 +1,17 @@
+<?php 
+	namespace Core\Controller;
+	/**
+	* AppController
+	*/
+	class AppController
+	{
+		
+		function __construct()
+		{
+			
+		}
+
+		public function index(){
+			echo "Olasd";
+		}
+	}
--- core/help/auto_load.class.php
+++ core/help/auto_load.class.php
@@ -1,6 +1,6 @@
 <?php 
 	
-	namespace Core;
+	namespace Core\Help;
 	/**
 	* AutoLoad
 	* @autor Jordan de Sousa Cassiano
