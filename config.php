ee0c1e04752ec5327945ca55f0d75e39954faaee
Jordan <jordan@emporioht.com.br>
2016-01-04 22:47:38 -0200

    Configruacao inicial

--- config.php
+++ config.php
@@ -1,7 +1,28 @@
 <?php 
-
+	
+	/** Caminho base para src em geral */
 	define("URL_BASE", "http://localhost/app");
 
+	/** caminho para includes */
 	define("DIR_BASE", __DIR__ );
 
+	/** tema da parte de site */
 	define("TEMA", "" );
+
+	/** Caminho base do tema */
+	define("DIR_TEMA","");
+
+	/** Modo da aplicacao */
+	define("DEBUG", true );
+
+	/** Diretorio de Upload */
+	define("DIR_UPLOAD", DIR_BASE."/uploads/" );
+
+	/** Host do banco*/
+	define("HOST", "localhost");
+
+	/** Nome do Banco */
+	define("BD","app");
+
+	/** Senha de acesso do banco */
+	define("SENHA","");
