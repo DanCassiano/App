126747d398e90d03142a102fe05a426ea7622fad
Jordan <jordan@emporioht.com.br>
2016-01-05 00:43:52 -0200

    Adicao de namespace

--- core/app.init.php
+++ core/app.init.php
@@ -10,13 +10,13 @@
 	use \Core;
 
 	// Definindo caminho root para o autoload
-	Core\AutoLoad::setRootDir( DIR_BASE );
+	Core\Help\AutoLoad::setRootDir( DIR_BASE );
 	
 	// Pastas contendo as classes
-	Core\AutoLoad::setDirretorios( array( 'core/class', 'core/controller' ));
+	Core\Help\AutoLoad::setDirretorios( array( 'core/class', 'core/controller' ));
 	
 	// Registrando o auto load
-	spl_autoload_register( array( 'Core\AutoLoad', 'registerLoad' ) );
+	spl_autoload_register( array( 'Core\Help\AutoLoad', 'registerLoad' ) );
 
 	$front = new Core\Controller();
 	$front->run();
