7c8050d0fda6639a4dbe4f3fb0bc504d2be92299
Jordan <jordan@emporioht.com.br>
2016-01-04 23:48:18 -0200

    Setando inicio da aplicação

--- core/app.init.php
+++ core/app.init.php
@@ -1,5 +1,22 @@
 <?php 
 	
-	require "class/auto_load.class.php";
+	if( defined("DEBUG") && DEBUG == true ) 
+		ini_set('display_errors', 1);
 
-	spl_autoload_register( array( 'AutoLoad', 'registerLoad' ) );
+
+	/** classe de autoload */
+	require "help/auto_load.class.php";
+
+	use \Core;
+
+	// Definindo caminho root para o autoload
+	Core\AutoLoad::setRootDir( DIR_BASE );
+	
+	// Pastas contendo as classes
+	Core\AutoLoad::setDirretorios( array( 'core/class', 'core/controller' ));
+	
+	// Registrando o auto load
+	spl_autoload_register( array( 'Core\AutoLoad', 'registerLoad' ) );
+
+	$front = new Core\Controller();
+	$front->run();
