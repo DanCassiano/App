c890d2f7bfc9e7359963294eefea159bd76baa29
Jordan <jordan@emporioht.com.br>
2016-01-05 23:16:48 -0200

    Adicionando novo diretorio e removendo espacos em branco

--- core/app.init.php
+++ core/app.init.php
@@ -3,18 +3,16 @@
 	if( defined("DEBUG") && DEBUG == true ) 
 		ini_set('display_errors', 1);
 
-
 	/** classe de autoload */
 	require "help/auto_load.class.php";
 
-	use \Core;
+	use \Core,
+		\Core\Controller;
 
 	// Definindo caminho root para o autoload
 	Core\Help\AutoLoad::setRootDir( DIR_BASE );
-	
 	// Pastas contendo as classes
-	Core\Help\AutoLoad::setDirretorios( array( 'core/class', 'core/controller' ));
-	
+	Core\Help\AutoLoad::setDirretorios( array("core", 'core/class', 'core/controller' ));
 	// Registrando o auto load
 	spl_autoload_register( array( 'Core\Help\AutoLoad', 'registerLoad' ) );
 
