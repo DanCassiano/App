0135447cc056bf8f659d83e8c2e92530e9d7c920
Jordan <jordan@emporioht.com.br>
2016-01-08 22:51:16 -0200

    Estrutura basica do Admin

---
+++ app-admin/view/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="<?php echo $app->lang ?>">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $app->titulo ?></title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="">
+	<?php $app->getCSS(); ?>
+</head>
+<body>
+	<div class="content">
+		<?php 
+			$app
+				->getHeader()
+				->getModulo()
+				->getFooter();
+		 ?>
+	</div>
+	<script src="<?php echo URL_BASE ?>/app-admin/assets/js/lib/angular.min.js"></script>
+	<script src="<?php echo URL_BASE ?>/app-admin/assets/js/app.js"></script>
+	<?php $app->getScripts() ?>
+</body>
+</html>
