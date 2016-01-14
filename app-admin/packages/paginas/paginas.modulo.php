3e490e2a7a319991d34077f8d5a7083c2aa2470b
Jordan <jordan@emporioht.com.br>
2016-01-14 21:28:52 -0200

    alteracao do nome da pasta de modulos para packages

---
+++ app-admin/packages/paginas/paginas.modulo.php
@@ -0,0 +1,52 @@
+
+<div class="janela janela-aba">
+	<div class="titulo" >
+		<div class="aba caret-right">
+			Paginas
+		</div>
+		<a href="#nova" class="action">
+			<i class="fa fa-plus icone "></i>
+			Nova pagina
+		</a>
+		
+		<div class="controle right">
+			<div class="pull-right icones">
+				<a href="#config">
+					<i class="fa fa-cog icone icone-invert icone-x2"></i>
+				</a>
+			</div>
+			<div class="pull-right">
+				<input type="text" value="" placeholder="Buscar..">
+			</div>
+			
+		</div>
+	</div>
+	<div class="corpo">
+		<table class="table">
+			<thead>
+				<tr>
+					<th class="t-small">
+						<input type="checkbox">
+					</th>
+					<th class="t-large" >Pagina(s)</th>
+					<th class="t-medium">Autor</th>
+					<th class="t-medium">Alterada em:</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>home</td>
+					<td>13-01-2016</td>
+					<td>Jordan</td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Contato</td>
+					<td>13-01-2016</td>
+					<td>Jordan</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
