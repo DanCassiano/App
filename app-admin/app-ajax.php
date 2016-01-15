70481800d08d5aac1e9da027a7b7a9c84d04891b
Jordan <jordan@emporioht.com.br>
2016-01-14 22:29:03 -0200

    Testando requisições

--- app-admin/app-ajax.php
+++ app-admin/app-ajax.php
@@ -7,5 +7,14 @@
  */
 
 
-	@header( 'Content-Type: text/html; charset=utf-8' ) );
+	@header( 'Content-Type: text/html; charset=utf-8' );
 	@header( 'X-Robots-Tag: noindex' );
+
+
+	$dados = array();
+
+	for( $i = 0; $i < rand(1,50); $i++ ) {
+		$dados[] = array( "id"=> $i, "package"=> "Lorem {$i}" );
+	}
+
+	echo json_encode( $dados );
