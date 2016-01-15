680ff847c37462ecdd8fab491ceafd7092177afa
Jordan <jordan@emporioht.com.br>
2016-01-14 22:29:50 -0200

    js de teste do modulo

---
+++ app-admin/packages/package/js/package.js
@@ -0,0 +1,35 @@
+angular
+	.module("App")
+		.controller("packageListController", packageListController )
+		.service("packageService", packageService );
+
+
+function packageListController( $scope, packageService ) {
+	function success(obj){
+		console.log('Data: ', obj);
+		$scope.packages = obj.data;
+	}
+	function error(err){
+		console.log('Erro: ', err);
+	}
+
+	packageService
+		.getList()
+		.then(success, error);
+	
+}
+
+function packageService($http) {
+	var REQ = {
+			url: 'http://localhost:3000/app/app-admin/app-ajax.php',
+			method: 'POST'
+		};
+
+		this.getList = function() {
+			return $http(REQ);
+		};
+	return this;
+}
+
+packageListController.$inject = ['$scope', 'packageService'];  
+packageService.$inject = ['$http'];  
