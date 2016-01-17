dd8205971c53a1e37266de8170566759c4f6ed8b
Jordan <jordan@emporioht.com.br>
2016-01-17 00:56:23 -0200

    Add pequena mudança para testes com ajax

--- app-admin/app-ajax.php
+++ app-admin/app-ajax.php
@@ -10,11 +10,22 @@
 	@header( 'Content-Type: text/html; charset=utf-8' );
 	@header( 'X-Robots-Tag: noindex' );
 
+	require "../app-include/ajax.php";
+		$dados = array();
 
-	$dados = array();
+	if( $_GET['acao'] == "eventos") {
 
-	for( $i = 0; $i < rand(1,50); $i++ ) {
-		$dados[] = array( "id"=> $i, "package"=> "Lorem {$i}" );
+		for( $i = 0; $i < rand(1,50); $i++ ) {
+			$dados[] = array( "id"=> $i, "title"=> "Lorem {$i}" );
+		}
+	}
+	else {
+
+
+		for( $i = 0; $i < rand(1,50); $i++ ) {
+			$dados[] = array( "id"=> $i, "package"=> "Lorem {$i}" );
+		}
+
 	}
 
 	echo json_encode( $dados );
