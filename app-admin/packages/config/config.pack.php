8ba417a473d8f87fe66f01191c196981e0b13d4b
Jordan <jordan@emporioht.com.br>
2016-01-17 00:55:58 -0200

    Package de config

---
+++ app-admin/packages/config/config.pack.php
@@ -0,0 +1,15 @@
+
+<div class="janela ">
+	<div class="titulo" >
+		Configurações
+		
+		<div class="controle right">
+			<div class="pull-right">
+				<input type="text" value="" placeholder="Buscar..">
+			</div>
+		</div>
+	</div>
+	<div class="corpo">
+		
+	</div>
+</div>
---
+++ app-admin/packages/config/init.php
@@ -0,0 +1,10 @@
+<?php
+; @modulo Config
+; @autor Jordan de Sousa Cassiano
+;
+/*
+[script]
+	config=paginas/js/paginas.js
+
+*/
+?>
