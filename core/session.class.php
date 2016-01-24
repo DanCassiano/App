2eda9b10c33ebd35c57b8d36ce1c173286880366
Jordan <jordan@emporioht.com.br>
2016-01-24 20:03:52 -0200

    Alterando forma de chamada da funcao

--- core/session.class.php
+++ core/session.class.php
@@ -43,24 +43,24 @@
 		public function start( $time = 3600, $nome = ""  ) {
 
 			try {
-			
-				if( empty( $nome ))
-					$nome = "app-".md5(md5( $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT'] ));
+				if( empty( session_id() ) ) {
+					session_set_cookie_params($time);
+					
+					if( empty( $nome ))
+						$nome = "app-".md5(md5( $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT'] ));
 
-				session_set_cookie_params($time);
-				session_name($nome);
-
-				if( empty( session_id() ) )
+					session_name($nome);
+					$this->nome = session_name();
+					$this->id 	= session_id();
+					$this->time = $time;
+					// Reseta o tempo para expirar a sessao
+					if (isset($_COOKIE[$this->nome]))
+						setcookie($this->nome, $_COOKIE[$this->nome], time() + $time, "/");
 					session_start();
-
-				$this->nome = session_name();
-				$this->id 	= session_id();
-				$this->time = $time;
-
-				// Reseta o tempo para expirar a sessao
-				if (isset($_COOKIE[$this->nome]))
-					setcookie($this->nome, $_COOKIE[$this->nome], time() + $time, "/");
-			} 
+				}
+				else
+					session_start();
+			}
 			catch (Exception $e) {
 				throw new \InvalidArgumentException( $e->getMessage() );
 			}
