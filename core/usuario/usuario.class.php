49ac66370229bafedbf81bdaf5e7843d0c8003cc
Jordan <jordan@emporioht.com.br>
2016-01-24 20:05:52 -0200

    Classe de usuario

--- core/usuario/usuario.class.php
+++ core/usuario/usuario.class.php
@@ -11,4 +11,19 @@
 		{
 			
 		}
+
+		public function get( $campos, $termos = "" , $binds = "" ) {
+			$pdo = new \Core\Pdo();
+			return $pdo->read("SELECT {$campos} FROM usuario {$termos}", $binds );
+		}
+
+		public function getUserLogado( $id ){
+			$d = $this->get("id,login","WHERE id=:id","id={$id}");
+			return $d[0];
+		}
+
+		public function lista( $d ) {
+			echo json_encode($this->get("id, login, data_cadastro, ativo","LIMIT 0,100 "));
+			// echo json_encode( array( array("id"=> 1, "login"=> "jordan")));
+		}
 	}
---
+++ core/usuario/usuario_dados.class.php
@@ -0,0 +1,13 @@
+<?php 
+	namespace Core\Usuario;
+	/**
+	* UsuarioDados
+	*/
+	class UsuarioDados
+	{
+		public $id;
+		public $login;
+		public $senha;
+		public $ativo;
+		public $id_pessoa;
+	}
