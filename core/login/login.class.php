7269967c77ad8cecefa15bbf2850d6cae2042d95
Jordan <jordan@emporioht.com.br>
2016-01-24 20:06:41 -0200

    Classe para controle de login

--- core/login/login.class.php
+++ core/login/login.class.php
@@ -4,8 +4,27 @@
 	/**
 	* Login
 	*/
-	class Login extends AppLogin
+	class Login
 	{
 		
+		public function logar( $dados ){
+			$user = new \Core\Usuario\Usuario();
+			$d =$user->get( "id"," WHERE login=:login ","login={$dados['login']}" );
+			if( $d ) {
+				$s = new \Core\Session();
+				// $s->start();
+				$s->id = $d[0]['id'];
+				echo json_encode( array("status"=> 1) );
+			}
+			else
+				echo json_encode( array("status"=> 0) );
+		}
+
+		public function logout( $id ) {
+
+			unset($_SESSION['id']);
+
+			echo json_encode( array("status"=>1));
+		}
 		
 	}
