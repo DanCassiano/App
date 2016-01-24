cf8a8e1f4d0bd12adfd2d88cc82096a22782bd3c
Jordan <jordan@emporioht.com.br>
2016-01-24 20:01:38 -0200

    ajustando declaracoes

--- app-admin/assets/js/app.js
+++ app-admin/assets/js/app.js
@@ -1,18 +1,25 @@
 
-angular.module("App", ['ui.calendar']);
+angular.module("App", []);
 angular
 	.module("App")
-		.controller("dropController", function($scope) {
-			
+		.controller("dropController",dropController)
+		.controller("menuController",menuController)
+		.service("loginService", loginService );
+
+		function dropController($scope, loginService){
 			$scope.aberto = true;
 
 			$scope.logChange = function( $event ) {
 				$event.preventDefault()
 				$scope.aberto = !$scope.aberto;
 			};
-			
-		})
-		.controller("menuController", function($scope) {
+
+			$scope.logoff = function( id ){
+				loginService.logoff(id);
+			}
+		}
+
+		function menuController( $scope ){
 			$scope.aberto = false;
 
 			$scope.menus = ["paginas","log","usuarios"];
@@ -21,4 +28,31 @@ angular
 				$event.preventDefault()
 				$scope.aberto = !$scope.aberto;
 			}
-		});
+		}
+
+		function loginService($http) {
+
+			function handleSuccess(res) {
+				if( res.data.status == 1 )
+					location.reload();
+			}
+				
+			function handleError(error) {
+				return function () {
+					return { success: false, message: error };
+				};
+			}
+				
+			this.logoff = function( id ) {
+				var p = $http({
+						url:'/app/app-admin/app-ajax.php',
+						method:'POST',
+						data: $.param({ controle: "login", metodo: "logout", id: id }),
+						headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+					});
+					p.then(handleSuccess, handleError('Error creating user'));
+			};
+			return this;
+		}
+		dropController.$inject = ['$scope', 'loginService'];  
+		loginService.$inject = ['$http'];
